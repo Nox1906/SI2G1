60c45cc7e266e880349bbcdf7f80c8475b15e772
Nox1906 <A46971@alunos.isel.pt>
2021-11-28 22:30:55 +0000

    tasks and drop tables

---
+++ droptable.sql
@@ -0,0 +1,12 @@
+USE L51NG1
+DROP TABLE dbo.EquipaIntervencao
+DROP TABLE dbo.IntervencaoPeriodica
+DROP TABLE dbo.Intervencao
+DROP TABLE dbo.EquipaFunc
+DROP TABLE dbo.Equipa
+DROP TABLE dbo.Historico
+DROP TABLE dbo.Ativos
+DROP TABLE dbo.Tipos
+DROP TABLE dbo.FunCompet
+DROP TABLE dbo.Funcionarios
+DROP TABLE dbo.Competencias
